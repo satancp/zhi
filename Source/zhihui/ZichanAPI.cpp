4a20a34dd50669fa1e0fe0aa318c574457c5339f
Chris <zx603852402@gmail.com>
2019-08-12 15:48:08 +0800

    finish zichan cpp&h

--- Source/zhihui/ZichanAPI.cpp
+++ Source/zhihui/ZichanAPI.cpp
@@ -2,4 +2,238 @@
 
 
 #include "ZichanAPI.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
+#include "Json.h"
+#include "JsonUtilities.h"
 
+void UZichanAPI::auth()
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject());
+	JsonObject->SetStringField(TEXT("username"), "chn");
+	JsonObject->SetStringField(TEXT("password"), "chn");
+	FString OutputString;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriter = TJsonWriterFactory<>::Create(&OutputString);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), JsonWriter);
+	TSharedRef<IHttpRequest> HttpReuest = FHttpModule::Get().CreateRequest();
+	HttpReuest->SetURL("http://localhost:8080/auth");
+	HttpReuest->SetVerb(TEXT("POST"));
+	HttpReuest->SetHeader("Content-Type", "application/json");
+	HttpReuest->SetContentAsString(OutputString);
+	HttpReuest->OnProcessRequestComplete().BindUObject(this, &UZichanAPI::authResponse);
+	HttpReuest->ProcessRequest();
+}
+
+void UZichanAPI::authResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	if (!HttpRequest.IsValid() || !HttpResponse.IsValid())
+	{
+		return;
+	}
+	else if (bSucceeded && EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+	{
+		TSharedPtr<FJsonObject> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			FString token = JsonObject->GetStringField("token");
+			this->token = token;
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *(token));
+		}
+	}
+}
+
+void UZichanAPI::getStatOnline()
+{
+	TSharedRef<IHttpRequest> HttpReuest = FHttpModule::Get().CreateRequest();
+	HttpReuest->SetURL("http://localhost:8080/api/asset/stat/online");
+	HttpReuest->SetVerb(TEXT("GET"));
+	HttpReuest->SetHeader(TEXT("Content-Type"), TEXT("application/x-www-form-urlencoded"));
+	HttpReuest->SetHeader(TEXT("Authorization"), TEXT("Bearer " + this->token));
+	HttpReuest->OnProcessRequestComplete().BindUObject(this, &UZichanAPI::getStatOnlineResponse);
+	HttpReuest->ProcessRequest();
+}
+
+void UZichanAPI::getStatOnlineResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	if (!HttpRequest.IsValid() || !HttpResponse.IsValid())
+	{
+		return;
+	}
+	else if (bSucceeded && EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+	{
+		TSharedPtr<FJsonValue> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			TArray<TSharedPtr<FJsonValue>> raw = JsonObject->AsArray();
+			TArray<FUStatOnline> data;
+			for (int32 index = 0; index < raw.Num(); index++)
+			{
+				TSharedPtr<FJsonObject> single = raw[index]->AsObject();
+				int32 oid = single->GetIntegerField("oid");
+				FString month = single->GetStringField("month");
+				int32 count = single->GetIntegerField("count");
+				FString statDate = single->GetStringField("statDate");
+				FUStatOnline temp = FUStatOnline(oid, month, count, statDate);
+				data.Add(temp);
+			}
+		}
+	}
+}
+
+void UZichanAPI::getStatPercentage()
+{
+	TSharedRef<IHttpRequest> HttpReuest = FHttpModule::Get().CreateRequest();
+	HttpReuest->SetURL("http://localhost:8080/api/asset/stat/percentage");
+	HttpReuest->SetVerb(TEXT("GET"));
+	HttpReuest->SetHeader(TEXT("Content-Type"), TEXT("application/x-www-form-urlencoded"));
+	HttpReuest->SetHeader(TEXT("Authorization"), TEXT("Bearer " + this->token));
+	HttpReuest->OnProcessRequestComplete().BindUObject(this, &UZichanAPI::getStatPercentageResponse);
+	HttpReuest->ProcessRequest();
+}
+
+void UZichanAPI::getStatPercentageResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	if (!HttpRequest.IsValid() || !HttpResponse.IsValid())
+	{
+		return;
+	}
+	else if (bSucceeded && EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+	{
+		TSharedPtr<FJsonValue> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			TArray<TSharedPtr<FJsonValue>> raw = JsonObject->AsArray();
+			TArray<FUStatPercentage> data;
+			for (int32 index = 0; index < raw.Num(); index++)
+			{
+				TSharedPtr<FJsonObject> single = raw[index]->AsObject();
+				FString type = single->GetStringField("type");
+				int32 count = single->GetIntegerField("count");
+				FUStatPercentage temp = FUStatPercentage(type, count);
+				data.Add(temp);
+			}
+		}
+	}
+}
+
+void UZichanAPI::getStatStatus()
+{
+	TSharedRef<IHttpRequest> HttpReuest = FHttpModule::Get().CreateRequest();
+	HttpReuest->SetURL("http://localhost:8080/api/asset/stat/status");
+	HttpReuest->SetVerb(TEXT("GET"));
+	HttpReuest->SetHeader(TEXT("Content-Type"), TEXT("application/x-www-form-urlencoded"));
+	HttpReuest->SetHeader(TEXT("Authorization"), TEXT("Bearer " + this->token));
+	HttpReuest->OnProcessRequestComplete().BindUObject(this, &UZichanAPI::getStatStatusResponse);
+	HttpReuest->ProcessRequest();
+}
+
+void UZichanAPI::getStatStatusResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	if (!HttpRequest.IsValid() || !HttpResponse.IsValid())
+	{
+		return;
+	}
+	else if (bSucceeded && EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+	{
+		TSharedPtr<FJsonValue> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			TArray<TSharedPtr<FJsonValue>> raw = JsonObject->AsArray();
+			TArray<FUStatStatus> data;
+			for (int32 index = 0; index < raw.Num(); index++)
+			{
+				TSharedPtr<FJsonObject> single = raw[index]->AsObject();
+				FString type = single->GetStringField("type");
+				int32 count = single->GetIntegerField("count");
+				FUStatStatus temp = FUStatStatus(type, count);
+				data.Add(temp);
+			}
+		}
+	}
+}
+
+void UZichanAPI::getStatType()
+{
+	TSharedRef<IHttpRequest> HttpReuest = FHttpModule::Get().CreateRequest();
+	HttpReuest->SetURL("http://localhost:8080/api/asset/stat/type");
+	HttpReuest->SetVerb(TEXT("GET"));
+	HttpReuest->SetHeader(TEXT("Content-Type"), TEXT("application/x-www-form-urlencoded"));
+	HttpReuest->SetHeader(TEXT("Authorization"), TEXT("Bearer " + this->token));
+	HttpReuest->OnProcessRequestComplete().BindUObject(this, &UZichanAPI::getStatTypeResponse);
+	HttpReuest->ProcessRequest();
+}
+
+void UZichanAPI::getStatTypeResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	if (!HttpRequest.IsValid() || !HttpResponse.IsValid())
+	{
+		return;
+	}
+	else if (bSucceeded && EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+	{
+		TSharedPtr<FJsonValue> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			TArray<TSharedPtr<FJsonValue>> raw = JsonObject->AsArray();
+			TArray<FUStatType> data;
+			for (int32 index = 0; index < raw.Num(); index++)
+			{
+				TSharedPtr<FJsonObject> single = raw[index]->AsObject();
+				FString type = single->GetStringField("type");
+				TArray<TSharedPtr<FJsonValue>> rawSub = single->GetArrayField("subTypes");
+				TArray<FString> subTypes;
+				for (int32 index2 = 0; index2 < rawSub.Num(); index2++)
+				{
+					subTypes.Add(rawSub[index2]->AsString());
+				}
+				FUStatType temp = FUStatType(type, subTypes);
+				data.Add(temp);
+			}
+		}
+	}
+}
+
+void UZichanAPI::getMaintain()
+{
+	TSharedRef<IHttpRequest> HttpReuest = FHttpModule::Get().CreateRequest();
+	HttpReuest->SetURL("http://localhost:8080/api/asset/maintain");
+	HttpReuest->SetVerb(TEXT("GET"));
+	HttpReuest->SetHeader(TEXT("Content-Type"), TEXT("application/x-www-form-urlencoded"));
+	HttpReuest->SetHeader(TEXT("Authorization"), TEXT("Bearer " + this->token));
+	HttpReuest->OnProcessRequestComplete().BindUObject(this, &UZichanAPI::getMaintainResponse);
+	HttpReuest->ProcessRequest();
+}
+
+void UZichanAPI::getMaintainResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	if (!HttpRequest.IsValid() || !HttpResponse.IsValid())
+	{
+		return;
+	}
+	else if (bSucceeded && EHttpResponseCodes::IsOk(HttpResponse->GetResponseCode()))
+	{
+		TSharedPtr<FJsonValue> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(HttpResponse->GetContentAsString());
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			TArray<TSharedPtr<FJsonValue>> raw = JsonObject->AsArray();
+			TArray<FUMaintain> data;
+			for (int32 index = 0; index < raw.Num(); index++)
+			{
+				TSharedPtr<FJsonObject> single = raw[index]->AsObject();
+				int32 oid = single->GetIntegerField("oid");
+				FString type = single->GetStringField("type");
+				FString region = single->GetStringField("region");
+				FString maintenance = single->GetStringField("maintenance");
+				FString planTime = single->GetStringField("planTime");
+				FString description = single->GetStringField("description");
+				FUMaintain temp = FUMaintain(oid, type, region, maintenance, planTime, description);
+				data.Add(temp);
+			}
+		}
+	}
+}
--- Source/zhihui/ZichanAPI.h
+++ Source/zhihui/ZichanAPI.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Http.h"
+#include "Json.h"
 #include "UObject/NoExportTypes.h"
 #include "ZichanAPI.generated.h"
 
@@ -12,6 +14,104 @@
 UCLASS()
 class ZHIHUI_API UZichanAPI : public UObject
 {
+	FString token = "";
+	void auth();
+	void authResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
 	GENERATED_BODY()
-	
+public:
+	UZichanAPI() {
+		auth();
+	};
+	UFUNCTION(BlueprintCallable)
+		void getStatOnline();
+	void getStatOnlineResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	UFUNCTION(BlueprintCallable)
+		void getStatPercentage();
+	void getStatPercentageResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	UFUNCTION(BlueprintCallable)
+		void getStatStatus();
+	void getStatStatusResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	UFUNCTION(BlueprintCallable)
+		void getStatType();
+	void getStatTypeResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	UFUNCTION(BlueprintCallable)
+		void getMaintain();
+	void getMaintainResponse(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+};
+
+USTRUCT()
+struct FUStatOnline {
+	GENERATED_BODY()
+		UPROPERTY() int32 oid;
+	UPROPERTY() FString month;
+	UPROPERTY() int32 count;
+	UPROPERTY() FString statDate;
+
+	FUStatOnline() {};
+	FUStatOnline(int32 o, FString m, int32 c, FString sd) {
+		oid = o;
+		month = m;
+		count = c;
+		statDate = sd;
+	};
+};
+
+USTRUCT()
+struct FUStatPercentage {
+	GENERATED_BODY()
+		UPROPERTY() FString type;
+	UPROPERTY() int32 count;
+
+	FUStatPercentage() {};
+	FUStatPercentage(FString t, int32 c) {
+		type = t;
+		count = c;
+	};
+};
+
+USTRUCT()
+struct FUStatStatus {
+	GENERATED_BODY()
+		UPROPERTY() FString type;
+	UPROPERTY() int32 count;
+
+	FUStatStatus() {};
+	FUStatStatus(FString t, int32 c) {
+		type = t;
+		count = c;
+	};
+};
+
+USTRUCT()
+struct FUStatType {
+	GENERATED_BODY()
+		UPROPERTY() FString type;
+	UPROPERTY() TArray<FString> subTypes;
+
+	FUStatType() {};
+	FUStatType(FString t, TArray<FString> st) {
+		type = t;
+		subTypes = st;
+	};
+};
+
+USTRUCT()
+struct FUMaintain {
+	GENERATED_BODY()
+		UPROPERTY() int32 oid;
+	UPROPERTY() FString type;
+	UPROPERTY() FString region;
+	UPROPERTY() FString maintenance;
+	UPROPERTY() FString planTime;
+	UPROPERTY() FString description;
+
+	FUMaintain() {};
+	FUMaintain(int32 o, FString t, FString r, FString m, FString p, FString d) {
+		oid = o;
+		type = t;
+		region = r;
+		maintenance = m;
+		planTime = p;
+		description = d;
+	};
 };
